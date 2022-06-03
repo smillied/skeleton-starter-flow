38345e8b409c572b7b6436c10e55a3fb497e024c
smillied <David.Smillie@thalesgroup.com.au>
2022-06-03 13:57:42 +0800

    Force SecurityConfiguration to scan for the missing Vaadin dependencies
    - This fails because RequestUtil has a dependency on the Spring Boot class, ServletRegistrationBean
    - The whole point of this is to avoid Spring Boot

--- src/main/java/org/vaadin/example/SecurityConfiguration.java
+++ src/main/java/org/vaadin/example/SecurityConfiguration.java
@@ -4,6 +4,7 @@
 package org.vaadin.example;
 
 import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
@@ -14,4 +15,5 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
  */
 @EnableWebSecurity
 @Configuration
+@ComponentScan(basePackages = {"com.vaadin.flow.spring.security"})
 public class SecurityConfiguration extends VaadinWebSecurityConfigurerAdapter {}
