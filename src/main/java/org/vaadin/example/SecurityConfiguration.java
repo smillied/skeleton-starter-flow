ec535f25b5e215291dcbd32ab92a965d1db720c7
smillied <David.Smillie@thalesgroup.com.au>
2022-06-03 16:59:37 +0800

    Changed to extend WebSecurityConfigurerAdapter instead of VaadinWebSecurityConfigurerAdapter
    - App runs but not seeing the expected login page

--- src/main/java/org/vaadin/example/SecurityConfiguration.java
+++ src/main/java/org/vaadin/example/SecurityConfiguration.java
@@ -3,9 +3,17 @@
  */
 package org.vaadin.example;
 
-import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
+import com.vaadin.flow.server.HandlerHelper;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 /**
  * Define Spring Security configuration
@@ -14,4 +22,36 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
  */
 @EnableWebSecurity
 @Configuration
-public class SecurityConfiguration extends VaadinWebSecurityConfigurerAdapter {}
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().requestMatchers(getDefaultWebSecurityIgnoreMatcher());
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth
+        // enable in memory based authentication with a user named "user" and "admin"
+        .inMemoryAuthentication()
+        .withUser("user")
+        .password("password")
+        .roles("USER")
+        .and()
+        .withUser("admin")
+        .password("password")
+        .roles("USER", "ADMIN");
+  }
+
+  /**
+   * Matcher for Vaadin static (public) resources.
+   *
+   * @return default {@link WebSecurity} ignore matcher
+   */
+  public static RequestMatcher getDefaultWebSecurityIgnoreMatcher() {
+    return new OrRequestMatcher(
+        Stream.of(HandlerHelper.getPublicResources())
+            .map(AntPathRequestMatcher::new)
+            .collect(Collectors.toList()));
+  }
+}
