b300f6bc6682e08fd816db26d4b2b030fd9eb436
smillied <David.Smillie@thalesgroup.com.au>
2022-06-03 12:55:52 +0800

    Just reformatting

--- src/main/java/org/vaadin/example/AppShell.java
+++ src/main/java/org/vaadin/example/AppShell.java
@@ -5,10 +5,9 @@ import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 
 /**
- * Use the @PWA annotation make the application installable on phones, tablets
- * and some desktop browsers.
+ * Use the @PWA annotation make the application installable on phones, tablets and some desktop
+ * browsers.
  */
 @PWA(name = "Project Base for Vaadin", shortName = "Project Base")
 @Theme("my-theme")
-public class AppShell implements AppShellConfigurator {
-}
+public class AppShell implements AppShellConfigurator {}
