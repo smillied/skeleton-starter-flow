83f35988c1df4f709199e866f5b1c461d1f4a4af
smillied <David.Smillie@thalesgroup.com.au>
2022-06-07 17:24:57 +0800

    Need to explicitly disable CSRF

--- src/main/java/org/vaadin/example/SecurityConfiguration.java
+++ src/main/java/org/vaadin/example/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -27,6 +28,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   public void configure(WebSecurity web) throws Exception {
     web.ignoring().requestMatchers(getDefaultWebSecurityIgnoreMatcher());
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable();
+    http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
   }
 
   @Override
